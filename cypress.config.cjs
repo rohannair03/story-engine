8c15d919f48e2430efd5498c240a510b62f0d797
rohannair03 <rohannair03@hotmail.com>
2026-03-10 17:20:55 -0700

    fix: convert cypress config to CommonJS to fix CI module resolution

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'cypress';
+const { defineConfig } = require('cypress');
 
-export default defineConfig({
+module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
     supportFile: 'cypress/support/e2e.js',
